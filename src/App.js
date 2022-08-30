64ff739177bd3479fca7f01c0a7a87db8ee372f3
Thierry <varelathierry@gmail.com>
2022-08-30 18:55:31 -0300

    requisito 2 renderizacao condicional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -9,17 +9,39 @@ import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
+  state = {
+    name: '',
+    btnOn: true,
+  };
+
+  handleName = ({ target }) => {
+    const minlength = 3;
+    this.setState({
+      name: target.value,
+      btnOn: (target.value.length < minlength),
+    });
+  };
+
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Route path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={ () => (<Login
+                { ...this.state }
+                handleName={ this.handleName }
+              />) }
+            />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
         </BrowserRouter>
       </div>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,65 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  state = {
+    loading: true,
+    waitApi: false,
+  };
+
+  handleChange = async () => {
+    const { name } = this.props;
+    this.setState({
+      loading: false,
+    }, async () => {
+      await createUser({ name });
+      this.setState({
+        waitApi: true,
+      });
+    });
+  };
+
+  redirectPage = () => {
+    const { waitApi } = this.state;
+    const value = <Redirect to="/search" />;
+    if (waitApi) {
+      return value;
+    }
+  };
+
   render() {
+    const { handleName, btnOn } = this.props;
+    const { loading } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        { loading ? (
+          <form>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              placeholder="Nome"
+              onChange={ handleName }
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ btnOn }
+              onClick={ this.handleChange }
+            >
+              Entrar
+            </button>
+          </form>
+        ) : (<p>Carregando...</p>)}
+        { this.redirectPage() }
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  handleName: PropTypes.func.isRequired,
+  btnOn: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+};
